6ed82dbec9be23152d96d99e509dbf09eb8b191e
Victor Byliev <ya.vic4592@yandex.ru>
2021-01-28 16:06:08 +0300

    Add project files.

---
+++ Host/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(WCF_Service_task.Service)))
+            {
+                host.Open();
+                Console.WriteLine("Host started");
+                Console.ReadLine();
+                host.Close();
+            }
+        }
+    }
+}
---
+++ WCF_Service_task/Service.svc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+
+namespace WCF_Service_task
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service : IService
+    {
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string GetNumber(int number)
+        {
+            var num = Math.Pow(number, 2).ToString();
+            return JsonSerializer.Serialize(num);
+            
+        }
+    }
+}
